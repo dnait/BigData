3b98e0b28e16420bab6bdc1ba16517313b454d5c
Katie <raokatie324@gmail.com>
2016-11-15 23:51:16 -0500

    Add case class demo

--- Scala/Functions/PatternMatching.scala
+++ Scala/Functions/PatternMatching.scala
@@ -8,7 +8,31 @@ object PatternMatching {
     println(matchAgain("pet"))         //many
     println(matchAgain(1))             //one
     println(matchAgain(200))           //scala.Int
+    
+    
+    //using Case classes
+    val alice = new Person("Alice", 23)
+    val bob = new Person("Bob", 32)
+    val charlie = new Person("Charlie", 32)
+    val mike = new Person("Mike", 50)
+    
+    for (person <- List(alice, bob, charlie, mike)) {
+      person match {
+        case Person("Alice", 23) => println("A girl")
+        case Person("Bob", 32) => println("Hi, Bob")
+        case Person(name, age) => println("Age:" + age + ", name: " + name + "?")
+      }
+    }
+    /*
+     * A girl
+     * Hi, Bob
+     * Age:32, name: Charlie?
+     * Age:50, name: Mike?
+     */
+
   }
+  
+  case class Person(name: String, age: Int)
   
   def matchTest(x: Int): String = x match {
     case 1 => "one"
