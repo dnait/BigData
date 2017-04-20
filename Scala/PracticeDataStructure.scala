dce15dac0e44bb310be9c378e4cae85acfec9fc6
Rao (Katie) Xu <dnait@users.noreply.github.com>
2017-04-19 23:20:16 -0400

    Update PracticeDataStructure.scala

--- Scala/PracticeDataStructure.scala
+++ Scala/PracticeDataStructure.scala
@@ -91,6 +91,8 @@ object PracticeDataStructure {
     //Create the List
     val x1 = List.range(1, 10)        //x1: List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
     val x2 = List.range(0, 10, 2)     //x2: List[Int] = List(0, 2, 4, 6, 8)
+    val x2 = List.range(1, 10, -2)    //List[]
+    val x2 = List.range(10, 1, -2)    //List(10, 8, 6, 4, 2)
     val x3 = List.fill(3)("foo")       //x3: List[java.lang.String] = List(foo, foo, foo)
     
     
