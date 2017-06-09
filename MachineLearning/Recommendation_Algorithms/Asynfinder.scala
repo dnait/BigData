6d42286c030b143cd5402a90296985c5b8ee0c11
Rao (Katie) Xu <dnait@users.noreply.github.com>
2017-06-08 23:50:26 -0400

    Word2Vec demo
    The example below demonstrates how to load a text file, parse it as an RDD of Seq[String], construct a Word2Vec instance and then fit a Word2VecModel with the input data. Finally, we display the top 40 synonyms of the specified word. 
    https://spark.apache.org/docs/1.2.0/mllib-feature-extraction.html

---
+++ MachineLearning/Recommendation_Algorithms/Asynfinder.scala
@@ -0,0 +1,33 @@
+package ScalaHandsOn
+
+import org.apache.spark._
+import org.apache.spark.rdd._
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.feature.Word2Vec
+
+object Asynfinder {
+  def main(args: Array[String]) {
+      // Set up a SparkContext named WordCount that runs locally using
+      // all available cores.
+
+      val conf = new SparkConf().setAppName("WordCount")
+      conf.setMaster("local[*]")
+      val sc = new SparkContext(conf)
+
+      // Create a RDD of lines of text in our book
+      val input = sc.textFile("text8").map(line => line.split(" ").toSeq)
+  
+      val word2vec = new Word2Vec()
+      
+      val model = word2vec.fit(input)
+      
+      val synonyms = model.findSynonyms("china", 40)
+      
+      for((synonym, cosineSimilarity) <- synonyms) {
+        println(s"$synonym $cosineSimilarity")
+      }
+      
+      sc.stop()
+
+  }
+}
